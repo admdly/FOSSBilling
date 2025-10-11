dd5b9af6d62ba44ae4760b851843e5da23c1f354
Yağızhan <hi@yagiz.dev>
2025-10-11 14:49:25 +0300

    Fixed tests

--- tests-legacy/modules/Servicelicense/Api/AdminTest.php
+++ tests-legacy/modules/Servicelicense/Api/AdminTest.php
@@ -110,9 +110,14 @@ class AdminTest extends \BBTestCase
             ->method('getExistingModelById')
             ->willReturn(new \Model_ClientOrder());
 
+        $validatorMock = $this->getMockBuilder('\\' . \FOSSBilling\Validate::class)->getMock();
+        $validatorMock->expects($this->atLeastOnce())
+            ->method('checkRequiredParamsForArray');
+
         $di = new \Pimple\Container();
+        $di['validator'] = $validatorMock;
         $di['db'] = $dbMock;
-        $di['mod_service'] = $di->protect(fn (): \PHPUnit\Framework\MockObject\MockObject => $orderServiceMock);
+        $di['mod_service'] = $di->protect(fn () => $orderServiceMock);
 
         $this->api->setDi($di);
 
@@ -134,9 +139,12 @@ class AdminTest extends \BBTestCase
             ->method('getExistingModelById')
             ->willReturn(new \Model_ClientOrder());
 
+        $validatorMock = $this->getMockBuilder('\\' . \FOSSBilling\Validate::class)->disableOriginalConstructor()->getMock();
+
         $di = new \Pimple\Container();
         $di['db'] = $dbMock;
-        $di['mod_service'] = $di->protect(fn (): \PHPUnit\Framework\MockObject\MockObject => $orderServiceMock);
+        $di['mod_service'] = $di->protect(fn () => $orderServiceMock);
+        $di['validator'] = $validatorMock;
 
         $this->api->setDi($di);
 
